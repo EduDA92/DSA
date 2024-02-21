d7aa2d4591b39f11e7bbc1a6648b1594c15a3c01
Edu <ediago92@gmail.com>
2024-02-21 16:24:27 +0100

    AmstrongNumbers

---
+++ BasicMath/AmstrongNumber.cpp
@@ -0,0 +1,23 @@
+bool checkArmstrong(int n){
+
+    int digits = 0;
+    int number = n;
+    int total = 0;
+	// first count the digits of the number
+
+    while(number != 0){
+        digits++;
+        number = number / 10;
+    }
+
+    // Reasign the number again
+    number = n;
+    while(number != 0){
+        
+        total += pow(number % 10, digits);
+        number = number / 10;
+    }
+
+    return total == n;
+
+}
